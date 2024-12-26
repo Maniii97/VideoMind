9488824edfa61f8053319fc7d7ac11de3dd4b11d
Mani <mjha34499@gmail.com>
2024-12-26 15:29:11 +0530

    made some changes for production

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -3,6 +3,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { colors } from '@/lib/constants/colors';
 
+import { cn } from '@/lib/utils';
+
 interface ChatInputProps {
   value: string;
   onChange: (value: string) => void;
