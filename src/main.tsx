df50dec47a2e883cd0d2789bdadb1a38abca4958
Mani <mjha34499@gmail.com>
2025-01-23 01:01:25 +0530

    feat : integrated backend api to make responses dynamic

--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,9 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
 
 root.render(
-  // <StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </StrictMode>
+  </StrictMode>
 );
